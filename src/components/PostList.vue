bcc59126810f94e339da3c70eec32f5d3311c66a
DanteDeGrave <vladimir.system@gmail.com>
2023-02-23 07:22:36 +0500

    wpa

---
+++ src/components/PostList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="posts.length">
+    <h3>Список постов</h3>
+    <ul class="post-list">
+      <post-item
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </ul>
+  </div>
+  <h2 v-else class="empty">Список постов пуст</h2>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem.vue";
+
+export default {
+  components: {PostItem},
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .post-list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  .empty {
+    color: red;
+  }
+</style>
